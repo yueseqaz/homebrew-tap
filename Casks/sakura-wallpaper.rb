d9a0f7a2555412b620603a687fb1d5064c66635a
Sakura <140417996+yueseqaz@users.noreply.github.com>
2026-04-16 18:38:32 +0800

    Update sakura-wallpaper.rb

--- Casks/sakura-wallpaper.rb
+++ Casks/sakura-wallpaper.rb
@@ -1,6 +1,6 @@
 cask "sakura-wallpaper" do
   version "1.0.0"
-  sha256 "e49d9998cd19a8e857cd3192c7926d8365264e3ad65a13f781571835605e5719"
+  sha256 "3488e34468baafe2d262c9cad806028576614a2280f4f5458ab0d1a60de0abdb"
 
   url "https://github.com/yueseqaz/SakuraWallpaper/releases/download/v#{version}/SakuraWallpaper.dmg"
   name "SakuraWallpaper"
